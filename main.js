dd2f0c749cbb0c222546e1f37d7bee74521a7ec5
llthompson <lisalacythompson+github@gmail.com>
2023-08-08 17:36:24 -0500

    many users

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 let userArray = [];
 
 async function fetchUsers() {
-    const response = await fetch('https://randomuser.me/api/?format=json');
+    const response = await fetch('https://randomuser.me/api/?results=60&nat=us');
     const data = await response.json(); 
     return data.results;
 }
 
 
-// fetchUsers()
-
-console.log('is there a user', userArray)
 
 const displayUser = () => {
-    console.log('hi')
     const aUser = document.getElementById('a-User')
-    console.log('is there a user 2', userArray)
+    // console.log('is there a user', userArray)
 
     userArray.forEach((user, index) => {
         console.log('here', user.name.first)
         const li = document.createElement('li')
-        // const picURL = URL.createObjectURL(`${user.picture.medium}`)
         const pic = document.createElement('img')
         pic.src = user.picture.large;
         li.appendChild(pic);
-        const text = document.createTextNode(` Name: ${user.name.first} ${user.name.last}`)
+        const text = document.createTextNode(`${user.name.first} ${user.name.last}`)
+        const moreInfo = document.createTextNode(` ${user.dob.date}`)
         li.appendChild(text)
+        li.appendChild(moreInfo)
         aUser.append(li)
     })
 }
@@ -35,4 +32,7 @@ const displayUser = () => {
     userArray.push(...users)
 displayUser()
 
+// console.log('final test', userArray)
+
 })()
+
